4b92bb59c2bbfb4d8c1c1a370ec83e2f5840b831
Tomersh97 <tomersh97@gmail.com>
2022-11-17 13:31:27 +0200

    3rd modification made

--- Tomer.java
+++ Tomer.java
@@ -1,7 +1,7 @@
 public class Tomer {
     public static void main(String[] args) {
         for(int i =1; i<=10;i++){
-            System.out.println("My name is Tomer and I've changed this code");
+            System.out.println("I've made my 3rd modification");
         }
     }
 }
